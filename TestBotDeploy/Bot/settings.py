75cfda9d2ec5bf35a1eec9ccf51fe84969aa9b4f
= <=>
2020-12-24 21:46:06 +0200

    DB chenges

--- TestBotDeploy/Bot/settings.py
+++ TestBotDeploy/Bot/settings.py
@@ -73,14 +73,26 @@ WSGI_APPLICATION = 'Bot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+if DEBUG:
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testbot',
+        'USER': 'testvic',
+        'PASSWORD': 'Sam!Plushka261',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
-
+   
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -118,3 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL='/media/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static_in_env')]
+STATIC_ROOT=os.path.join(BASE_DIR,'static_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')``
