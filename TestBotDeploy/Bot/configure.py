ffc69ad2326103e2575615af75363bd4ec42804c
= <=>
2021-01-21 17:20:23 +0200

    conf

--- TestBotDeploy/Bot/configure.py
+++ TestBotDeploy/Bot/configure.py
@@ -1,4 +1,4 @@
 config={
-    'name':'CocktailExpressTLVtest',
-    'token':'1326618661:AAHHccJpWbOawgOLFxmnqJWKoeNkyYZW3S8'
+    'name':'CocktailExpress',
+    'token':'1294935121:AAFQ7_OOmqt0f4FTSJVfse5uvEsNgp2fm2A'
       }
